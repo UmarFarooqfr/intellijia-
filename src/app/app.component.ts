f3d40df4f3e4c365bd0c7a695cd53c9c47f3c264
farrukh0913 <farrukh.khan0913@gmail.com>
2023-05-29 16:02:03 +0500

    flex layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'headers';
+  isDashboardPage: boolean = false;
+  intervalField = new FormControl('');
+  isAdminSide = new FormControl('');
+  alarmField = new FormControl('');
+  refreshIntervals = [{name: 'a', value: '1'}, {name: 'b', value: '2'}];
+  countriesLanguages = [{name: 'a', code: '1'}, {name: 'b', code: '2'}];
+  notifications = [{name: 'a', code: '1', content: '', eventTime: '123'}, {name: 'b', code: '2', content: '', eventTime: '123'}];
+
+  constructor(){
+    this.isDashboardPage = true;
+  }
+
+  onRefreshChange(){}
+
+  onIntervalChange(e: any){
+    console.log('e: ', e);
+  }
+
+  openNotification(){}
+
+  logOut(){}
+
 }
