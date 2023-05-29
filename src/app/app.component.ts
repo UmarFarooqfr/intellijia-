226258b46af48e9900d85fadb5a87de12fcf40e5
farrukh0913 <farrukh.khan0913@gmail.com>
2023-05-29 23:36:45 +0500

    header 2nd portion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,28 @@ export class AppComponent {
   intervalField = new FormControl('');
   isAdminSide = new FormControl('');
   alarmField = new FormControl('');
-  refreshIntervals = [{name: 'a', value: '1'}, {name: 'b', value: '2'}];
-  countriesLanguages = [{name: 'a', code: '1'}, {name: 'b', code: '2'}];
-  notifications = [{name: 'a', code: '1', content: '', eventTime: '123'}, {name: 'b', code: '2', content: '', eventTime: '123'}];
+  refreshIntervals = [{ name: 'a', value: '1' }, { name: 'b', value: '2' }];
+  countriesLanguages = [{ name: 'a', code: '1' }, { name: 'b', code: '2' }];
+  notifications = [{ name: 'a', code: '1', content: '', eventTime: '123' }, { name: 'b', code: '2', content: '', eventTime: '123' }];
+  selectedTime: string = '';
 
-  constructor(){
+
+  constructor() {
     this.isDashboardPage = true;
   }
 
-  onRefreshChange(){}
+  onRefreshChange() { }
 
-  onIntervalChange(e: any){
+  onMenuItemTime(selectedTime: string) {
+    this.selectedTime = selectedTime
+  }
+
+  onIntervalChange(e: any) {
     console.log('e: ', e);
   }
 
-  openNotification(){}
+  openNotification() { }
 
-  logOut(){}
+  logOut() { }
 
 }
